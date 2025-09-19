66a031377f135ffe27afa00255f0ff5c8774f97b
alexfirdouss-debug <alexfirdouss@gmail.com>
2025-09-19 21:15:32 +0530

    Create ui_helpers.dart

---
+++ lib/lib/app/lib/app/lib/app/lib/models/lib/services/lib/services/lib/services/lib/services/lib/services/lib/services/lib/ui/common/lib/ui/common/lib/ui/common/ui_helpers.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UiHelpers {
+  static SizedBox verticalSpaceSmall = SizedBox(height: 8);
+  static SizedBox verticalSpaceMedium = SizedBox(height: 20);
+  static SizedBox verticalSpaceLarge = SizedBox(height: 40);
+
+  static SizedBox horizontalSpaceSmall = SizedBox(width: 8);
+  static SizedBox horizontalSpaceMedium = SizedBox(width: 20);
+}
