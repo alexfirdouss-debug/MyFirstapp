bd05faac2a1349ee5ba9f008b0f3be8dfd8c9ede
alexfirdouss-debug <alexfirdouss@gmail.com>
2025-09-19 21:02:34 +0530

    Create app.locator.dart

---
+++ lib/lib/app/lib/app/app.locator.dart
@@ -0,0 +1,12 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+import 'package:get_it/get_it.dart';
+import 'package:stacked/stacked.dart';
+
+final locator = GetIt.instance;
+
+void setupLocator() {
+  // Register services here
+  // For example:
+  // locator.registerLazySingleton(() => AuthenticationService());
+}
