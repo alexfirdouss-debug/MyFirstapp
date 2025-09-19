bb5d73e5873d10e1057ecc5104dbdac769140008
alexfirdouss-debug <alexfirdouss@gmail.com>
2025-09-19 21:13:36 +0530

    Create app_colors.dart

---
+++ lib/lib/app/lib/app/lib/app/lib/models/lib/services/lib/services/lib/services/lib/services/lib/services/lib/services/lib/ui/common/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF32a852);
+  static const secondary = Color(0xFF3289a8);
+  static const accent = Color(0xFFa832a8);
+  static const glass = Colors.white54;
+  static const background = Color(0xFF121212);
+  static const card = Color(0xFF1E1E1E);
+}
