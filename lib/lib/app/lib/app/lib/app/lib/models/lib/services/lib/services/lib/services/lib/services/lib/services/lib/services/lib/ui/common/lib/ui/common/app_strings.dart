288b71ca7778c65775bcf6b0fe634a3cf535d2a8
alexfirdouss-debug <alexfirdouss@gmail.com>
2025-09-19 21:15:06 +0530

    Create app_strings.dart

---
+++ lib/lib/app/lib/app/lib/app/lib/models/lib/services/lib/services/lib/services/lib/services/lib/services/lib/services/lib/ui/common/lib/ui/common/app_strings.dart
@@ -0,0 +1,9 @@
+class AppStrings {
+  static const onboardingTitle1 = "Say goodbye to endless distractions";
+  static const onboardingTitle2 = "Build your unstoppable focus streaks";
+  static const onboardingTitle3 = "Let AI guide your journey to success";
+
+  static const welcomeGreeting = "Welcome back, ";
+  static const dailyQuote = "Your daily focus quote:";
+  static const startSession = "Start Focus Session";
+}
